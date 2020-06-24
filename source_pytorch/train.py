a6d355377a3a79191714822bf0aa12f51e81c588
supulij <54053292+supulij@users.noreply.github.com>
2020-06-24 11:42:26 +0100

    Update train.py

--- source_pytorch/train.py
+++ source_pytorch/train.py
@@ -92,7 +92,7 @@ def train(model, train_loader, epochs, criterion, optimizer, device):
         print("Epoch: {}, Loss: {}".format(epoch, total_loss / len(train_loader)))
 
 
-## TODO: Complete the main code
+## The main code
 if __name__ == '__main__':
     
     # All of the model parameters and training parameters are sent as arguments
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     
-    ## TODO: Add args for the three model parameters: input_features, hidden_dim, output_dim
+    ## args for the three model parameters: input_features, hidden_dim, output_dim
     # Model Parameters
     parser.add_argument('--input_features', type=int, default=3, metavar='IN',
                         help='size of the input neurons  (default: 3)')
@@ -137,23 +137,21 @@ if __name__ == '__main__':
 
     # Load the training data.
     train_loader = _get_train_data_loader(args.batch_size, args.data_dir)
-
-
-    ## --- Your code here --- ##
+  
     
-    ## TODO:  Build the model by passing in the input params
+    # Build the model by passing in the input params
     # To get params from the parser, call args.argument_name, ex. args.epochs or ards.hidden_dim
-    # Don't forget to move your model .to(device) to move to GPU , if appropriate
+    
     model = BinaryClassifier(args.input_features, args.hidden_dim, args.output_dim).to(device)
 
-    ## TODO: Define an optimizer and loss function for training
+    # Define an optimizer and loss function for training
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = nn.BCELoss()
 
     # Trains the model (given line of code, which calls the above training function)
     train(model, train_loader, args.epochs, criterion, optimizer, device)
 
-    ## TODO: complete in the model_info by adding three argument names, the first is given
+    # Complete in the model_info by adding three argument names, the first is given
     # Keep the keys of this dictionary as they are 
     model_info_path = os.path.join(args.model_dir, 'model_info.pth')
     with open(model_info_path, 'wb') as f:
@@ -162,10 +160,8 @@ if __name__ == '__main__':
             'hidden_dim': args.hidden_dim,
             'output_dim': args.output_dim,
         }
-        torch.save(model_info, f)
-        
-    ## --- End of your code  --- ##
-    
+        torch.save(model_info, f)       
+      
 
 	# Save the model parameters
     model_path = os.path.join(args.model_dir, 'model.pth')
